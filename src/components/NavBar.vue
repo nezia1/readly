1806e11a44f01da4450701dcece2ad48b976b0c4
Anthony Rodriguez <anthony.rdrgz3@eduge.ch>
2019-12-28 17:56:05 +0100

    add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-navbar class="is-spaced">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <h1 class="is-size-5">Readly</h1>
+      </b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">Log in</a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+
+<style>
+</style>
