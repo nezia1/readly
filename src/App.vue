b857ef6631bd3c2e75ff3aff452ce53f4d6c97f6
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-03 19:27:14 +0100

    move dashboard to template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="app">
     <nav-bar />
-    <router-view />
+    <div class="container is-fluid">
+      <div class="columns">
+        <Dashboard class="column is-one-quarter" />
+        <router-view class="column" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import NavBar from '@/components/NavBar'
+import Dashboard from '@/components/Dashboard'
+
 export default {
   name: 'app',
   components: {
-    NavBar
+    NavBar,
+    Dashboard
   }
 }
 </script>
