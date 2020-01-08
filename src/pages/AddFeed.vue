f0f12e50d1cbe050c3a687b71b24508ff910ceae
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-08 15:00:27 +0100

    add rss form

--- src/pages/AddFeed.vue
+++ src/pages/AddFeed.vue
@@ -1,10 +1,22 @@
 <template>
-  <div></div>
+  <div class="columns">
+    <div class="column is-three-fifths">
+      <b-field label="RSS Link">
+        <b-input placeholder="Enter a valid RSS link" v-model="rssLink" icon="rss"></b-input>
+      </b-field>
+      <b-button type="is-primary">Add</b-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'add-feed',
+  data: function () {
+    return {
+      rssLink: ''
+    }
+  }
 }
 </script>
 
