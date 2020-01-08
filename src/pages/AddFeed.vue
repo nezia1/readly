33a1c53544ff30fa8654514b872a2072d22303f8
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-08 15:32:22 +0100

    add control div to fields to group them

--- src/pages/AddFeed.vue
+++ src/pages/AddFeed.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="columns">
     <div class="column is-three-fifths">
-      <b-field label="RSS Link">
-        <b-input placeholder="Enter a valid RSS link" v-model="rssLink" icon="rss"></b-input>
-      </b-field>
-      <b-button type="is-primary">Add</b-button>
+      <div class="control">
+        <b-field label="RSS Link">
+          <b-input placeholder="Enter a valid RSS link" v-model="rssLink" icon="rss"></b-input>
+        </b-field>
+        <b-button type="is-primary">Add</b-button>
+      </div>
     </div>
   </div>
 </template>
