8c97bab7b25ab6403894c53e51e05cff2f3619f4
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-08 15:23:25 +0100

    remove navbar fluid

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
-  <b-navbar class="is-spaced">
+  <!-- TODO: remove burger menu -->
+  <b-navbar mobile-burger="false">
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <h1 class="is-size-3">Readly</h1>
