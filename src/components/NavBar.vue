71e9beccffa40e5562d7afad46599f8446a43d13
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-08 15:00:11 +0100

    remove login buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,16 +5,6 @@
         <h1 class="is-size-3">Readly</h1>
       </b-navbar-item>
     </template>
-    <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">Log in</a>
-        </div>
-      </b-navbar-item>
-    </template>
   </b-navbar>
 </template>
 
