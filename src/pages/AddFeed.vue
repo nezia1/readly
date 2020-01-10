d59b051f33e8e3c95dc00cf691a320775738446c
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-10 16:50:00 +0100

    Add message confirmation when adding a feed

--- src/pages/AddFeed.vue
+++ src/pages/AddFeed.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="columns">
+  <div class="columns is-multiline">
     <div class="column is-three-fifths">
       <div class="control">
         <b-field label="RSS Link">
@@ -7,6 +7,10 @@
         </b-field>
         <b-button type="is-primary">Add</b-button>
       </div>
+    </div>
+    <div class="column is-two-fifths"></div>
+    <div class="column is-two-fifths">
+      <b-message type="is-success" has-icon>RSS feed was successfully added!</b-message>
     </div>
   </div>
 </template>
