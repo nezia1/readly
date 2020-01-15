293b9595a48da14c19be861b18f6a68b04e6db76
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-15 22:18:46 +0100

    Add list of articles

--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -1,5 +1,18 @@
 <template>
-<div></div>
+  <div class="columns is-multiline">
+    <div class="column is-half" v-for="article in articles" v-bind:key="article.guid">
+      <div class="card">
+        <div class="card-content">
+          <p class="title">{{article.title}}</p>
+          <p class="subtitle">{{article.creator}}</p>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-item">Read</div>
+          <div class="card-footer-item">Mark as read</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,10 +22,14 @@ export default {
     return {
 
     }
+  },
+  computed: {
+    articles: function () {
+      return this.$store.state.feeds.find(feed => feed.id === this.$route.params.id).items
+    }
   }
 }
 </script>
 
 <style>
-
 </style>
