724765c1b0c4abc3a5c9e2f595ff65e68e51596d
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-15 14:40:13 +0100

    Add TODO

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -7,6 +7,7 @@
           Feeds
           <b-icon class="is-pulled-right" :icon="isActive ? 'menu-up' : 'menu-down'"></b-icon>
         </template>
+        <!-- TODO: make nicer URLs for feeds using feed title and still keep the id to display articles -->
         <b-menu-item v-for="feed in feeds" v-bind:key="feed.id" v-bind:label="feed.title" tag="router-link" :to="{path: `/feeds/${feed.id}`}"></b-menu-item>
       </b-menu-item>
       <b-menu-item icon="plus" label="Add a feed" tag="router-link" :to="{path: '/add'}"></b-menu-item>
@@ -16,6 +17,7 @@
 
 <script>
 import { mapState } from 'vuex'
+
 export default {
   name: 'dashboard',
   data: function () {
