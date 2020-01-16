d6b8d94897dbe45676610c4365e0990514cc78ca
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-16 14:39:58 +0100

    Add TODO

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <b-menu>
+    <!-- TODO: Make menu sticky -->
     <b-menu-list label="Feeds">
       <b-menu-item icon="newspaper" label="New articles"></b-menu-item>
       <b-menu-item icon="rss" :active="isActive" :expanded="isActive" @click="isActive = !isActive">
@@ -8,7 +9,13 @@
           <b-icon class="is-pulled-right" :icon="isActive ? 'menu-up' : 'menu-down'"></b-icon>
         </template>
         <!-- TODO: make nicer URLs for feeds using feed title and still keep the id to display articles -->
-        <b-menu-item v-for="feed in feeds" v-bind:key="feed.id" v-bind:label="feed.title" tag="router-link" :to="{path: `/feeds/${feed.id}`}"></b-menu-item>
+        <b-menu-item
+          v-for="feed in feeds"
+          v-bind:key="feed.id"
+          v-bind:label="feed.title"
+          tag="router-link"
+          :to="{path: `/feeds/${feed.id}`}"
+        ></b-menu-item>
       </b-menu-item>
       <b-menu-item icon="plus" label="Add a feed" tag="router-link" :to="{path: '/add'}"></b-menu-item>
     </b-menu-list>
