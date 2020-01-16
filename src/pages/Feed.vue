e2f0ad8fd3b34ffcc22567aa785fbb5c643fb80a
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-16 12:53:42 +0100

    Add article modal

--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -7,9 +7,24 @@
           <p class="subtitle">{{article.creator}}</p>
         </div>
         <div class="card-footer">
-          <div class="card-footer-item">Read</div>
-          <div class="card-footer-item">Mark as read</div>
+          <a class="card-footer-item" v-on:click="openArticle(article)">Read</a>
+          <a class="card-footer-item">Save</a>
         </div>
+      </div>
+    </div>
+    <div v-bind:class="{'modal': true, 'is-active': isModalOpened }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{ openedArticle.title }}</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="content" v-html="openedArticle.content"></div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button">Close</button>
+        </footer>
       </div>
     </div>
   </div>
@@ -20,12 +35,24 @@ export default {
   name: 'feed',
   data: function () {
     return {
-
+      isModalOpened: false,
+      openedArticle: {}
     }
   },
   computed: {
     articles: function () {
       return this.$store.state.feeds.find(feed => feed.id === this.$route.params.id).items
+    }
+  },
+  methods: {
+    openArticle: function (clickedArticle) {
+      this.isModalOpened = true
+      this.openedArticle = clickedArticle
+    },
+    parseArticle: function (article) {
+      const domparser = new DOMParser()
+      const parsedArticle = domparser.parseFromString(article.content)
+      return parsedArticle
     }
   }
 }
