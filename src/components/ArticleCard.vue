bbc618536e3491de74dbdec1a52e862ce635944b
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-17 10:53:40 +0100

    Add ArticleCard to cleanup the code
    Same idea as last commit

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <p class="title">{{article.title}}</p>
+      <p class="subtitle">{{article.creator}}</p>
+    </div>
+    <div class="card-footer">
+      <a class="card-footer-item" v-on:click.stop="$emit('open-article')">Read</a>
+      <a class="card-footer-item">Save</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-card',
+  props: ['article']
+}
+</script>
+
+<style>
+</style>
