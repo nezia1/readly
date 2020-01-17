acd442b933ed40243bd464fb0cd92d315a3b342b
Anthony Rodriguez <anthony@nezia.xyz>
2020-01-17 10:39:40 +0100

    Add FeedModal component
    Put the Feed modal into its own component to make the code cleaner

---
+++ src/components/FeedModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ openedArticle.title }}</p>
+        <button class="delete" aria-label="close" v-on:click="$emit('close-article')"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="content" v-html="openedArticle.content"></div>
+      </section>
+      <footer class="modal-card-foot">
+        <a class="button is-link" v-bind:href="openedArticle.link" target="_blank">Read on website</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'feed-modal',
+  props: ['openedArticle']
+}
+</script>
+
+<style>
+</style>
