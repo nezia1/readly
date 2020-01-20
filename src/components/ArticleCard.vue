023ed4711e2e4b96fe2eb777b9be72790520229e
Anthony Rodriguez <anthony.lol2001@gmail.com>
2020-01-20 08:48:39 +0100

    Add date on article card

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -3,6 +3,7 @@
     <div class="card-content">
       <p class="title">{{article.title}}</p>
       <p class="subtitle">{{article.creator}}</p>
+      <p class="subtitle is-6">{{getReadableDate(article.isoDate)}}</p>
     </div>
     <div class="card-footer">
       <a class="card-footer-item" v-on:click.stop="$emit('open-article')">Read</a>
@@ -12,9 +13,16 @@
 </template>
 
 <script>
+const date = new Date()
+
 export default {
   name: 'article-card',
-  props: ['article']
+  props: ['article'],
+  methods: {
+    getReadableDate: function (isoDate) {
+      return date.toDateString(isoDate)
+    }
+  }
 }
 </script>
 
